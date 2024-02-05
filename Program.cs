03336d763e449e9cad3dbd02dd380b8df63833aa
Resul Quluzade <quluzade.resul13@mail.ru>
2024-02-05 20:58:34 +0400

    Bootstrap project

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using CommandApp.App;
+using CommandApp.Command;
+
+namespace HabitApp;
+
+public static class Program
+{
+    public static void Main()
+    {
+        ICommandCollection commands = new DefaultCommandCollection();
+        RegisterCommands(commands);
+
+        IApp app = new DefaultApp(commands);
+        app.Start();
+    }
+
+    private static void RegisterCommands(ICommandCollection commands)
+    {
+
+    }
+}
