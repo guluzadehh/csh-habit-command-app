15d76100c34f0c6081ceba0ab49ca297a9086202
Resul Quluzade <quluzade.resul13@mail.ru>
2024-02-07 00:33:52 +0400

    Property method fix

--- Entities/HabitEntity.cs
+++ Entities/HabitEntity.cs
@@ -7,7 +7,7 @@ namespace HabitApp
         public override string TableName { get; } = "habits";
         public EntityField<string> Name { get; } = new("name", "");
         public EntityField<string> Description { get; } = new("description", "");
-        public EntityField<string> UnitName { get; set; } = new("unit_name", "");
+        public EntityField<string> UnitName { get; } = new("unit_name", "");
 
     }
 }
